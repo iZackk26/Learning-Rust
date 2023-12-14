2ff0a53aaab3eeaa9a9214a5e43225a367bd153e
iZackk26 <newxstorecr@gmail.com>
2023-12-14 16:25:44 -0600

    feat: Adding first manipulation with tuples

--- Day_2/src/main.rs
+++ Day_2/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let x: i32 = 5;
-    let y: i32 = 6;
-    
-    print!("{} + {} = {}", x, y, x + y)
+    // Tuple with three elements
+    let my_tuple = (10, 20, 30);
+
+    // Slice with four elements
+    let my_slice = &[40, 50, 60, 70];
+
+    // Your task:
+    // 1. Extract the second element from the tuple and bind it to variable `a`.
+    // 2. Use destructuring assignment to extract the last two elements from the slice and bind them to variables `b` and `c`.
+    // 3. Uncomment and fill in the blank in the assert_eq! macro to make the code work.
+
+    let a = my_tuple.1;
+    let [_, b, c @ ..] = my_slice;
+
+    // Fill in the blank to make the code work
+    assert_eq!((a, *b, c[0]), (20, 50, 60));
+
+    println!("Success!");
 }
+
